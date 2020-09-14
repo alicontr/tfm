60fc95805a19ec0612df09216ec9d39ffeeacea0
alicontr <45132873+alicontr@users.noreply.github.com>
2020-09-14 15:52:54 +0200

    Add files via upload

---
+++ credenciales_twitter.py
@@ -0,0 +1,19 @@
+# Credenciales para poder escuchar tweets. De esta manera se mantienen separados
+# del programa escuchador
+
+# Estos valores salen de la configuración de la aplicación
+CONSUMER_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+CONSUMER_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    
+# Estos valores hay que obtenerlos a mano usando el navegador
+# 1) Visitar la URL de verificación y conseguir el PIN
+#       auth.get_authorization_url()
+# 2) Introducir el PIN en auth para obtener access_token y access_token_secret
+#       auth.get_access_token(PIN)
+# 3) Extraer access_token y access_token_secret y almacenarlos
+#       auth.access_token
+#       auth.access_token_secret
+ACCESS_TOKEN = 'VqDWQaVsvzZWpr2Oom8vmcduFfCXYIhqy5wcmXY5ujBwX'
+ACCESS_TOKEN_SECRET = '1613902262-njYxlbRm9M2R70Yjrm3XNw8twNcjHGnI4RveTqm'
+
+
